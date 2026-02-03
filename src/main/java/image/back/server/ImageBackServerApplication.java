280f54385704390ec57880b12f6639c0085cfd48
김도영 <zz01064842675@gmail.com>
2026-02-03 11:07:02 +0900

    feat: 초기 Spring Boot 프로젝트 설정 및 Gradle 빌드 구성

---
+++ src/main/java/image/back/server/ImageBackServerApplication.java
@@ -0,0 +1,13 @@
+package image.back.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageBackServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageBackServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/image/back/server/ImageBackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package image.back.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageBackServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
